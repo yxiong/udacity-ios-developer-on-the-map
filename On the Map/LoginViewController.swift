e53ea9de5920f6e85aecd51f5114a00d67b3f8f8
Ying Xiong <ying@xiongs.org>
2015-10-24 21:05:13 -0700

    Login and get session id with hardcoded username and password

--- On the Map/LoginViewController.swift
+++ On the Map/LoginViewController.swift
@@ -27,6 +27,30 @@ class LoginViewController: UIViewController {
             print("Email and/or password field is empty.")
             return
         }
+        let methodParameters: [String: String!] = [
+            "username": emailTextField.text,
+            "password": passwordTextField.text
+        ]
+        let urlString = "https://www.udacity.com/api/session"
+        let url = NSURL(string: urlString)!
+        let request = NSMutableURLRequest(URL: url)
+        request.HTTPMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.HTTPBody = "{\"udacity\": {\"username\": \"ying@xiongs.org\", \"password\":\"******\"}}".dataUsingEncoding(NSUTF8StringEncoding)
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request) { data, response, error in
+            if error != nil {
+                print(error)
+            }
+            guard let data = data else {
+                print("No data was returned by the request!")
+                return
+            }
+            let newData = data.subdataWithRange(NSMakeRange(5, data.length-5))
+            print(NSString(data: newData, encoding: NSUTF8StringEncoding))
+        }
+        task.resume()
         print("Email: ", emailTextField.text)
         print("Passord: ", passwordTextField.text)
     }
