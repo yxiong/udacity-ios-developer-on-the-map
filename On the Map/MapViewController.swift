4461abe807e6f7d2a23485c7b500842d1038dd3a
Ying Xiong <ying@xiongs.org>
2015-10-25 08:26:56 -0700

    Add a map view

---
+++ On the Map/MapViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MapViewController.swift
+//  On the Map
+//
+//  Created by Ying Xiong on 10/25/15.
+//  Copyright © 2015 Ying Xiong. All rights reserved.
+//
+
+import MapKit
+import UIKit
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+}
