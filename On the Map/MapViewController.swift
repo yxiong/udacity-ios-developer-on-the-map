1c13750d43273d3a55d99369e7f7dd299b1945ba
Ying Xiong <ying@xiongs.org>
2015-10-25 20:44:11 -0700

    Retrieve map data with account key

--- On the Map/MapViewController.swift
+++ On the Map/MapViewController.swift
@@ -16,7 +16,22 @@ class MapViewController: UIViewController, MKMapViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
-        print("Account Key:", appDelegate.accountKey)
-        print("Session ID:", appDelegate.sessionId)
+
+        let request = NSMutableURLRequest(URL: NSURL(string: NSString(format: "https://www.udacity.com/api/users/%@", appDelegate.accountKey!) as String)!)
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request) { data, response, error in// Error checking of response.
+            guard error == nil else {
+                print("Error returned by request", error)
+                return
+            }
+            guard let data = data else {
+                print("No data was returned by the request!")
+                return
+            }
+            // Handling special format of response data (skipping the first 5 characters).
+            let newData = data.subdataWithRange(NSMakeRange(5, data.length - 5)) /* subset response data! */
+            print(NSString(data: newData, encoding: NSUTF8StringEncoding))
+        }
+        task.resume()
     }
 }
