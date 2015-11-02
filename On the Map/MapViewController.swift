a7c8689f02273daeb278f7318530b89ad07b9779
Ying Xiong <ying@xiongs.org>
2015-11-01 20:37:03 -0800

    Add submitNewPin function

--- On the Map/MapViewController.swift
+++ On the Map/MapViewController.swift
@@ -74,6 +74,24 @@ class MapViewController: UIViewController, MKMapViewDelegate {
         print("Add button pushed")
     }
     
+    func submitNewPin() {
+        let request = NSMutableURLRequest(URL: NSURL(string: "https://api.parse.com/1/classes/StudentLocation")!)
+        request.HTTPMethod = "POST"
+        request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
+        request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.HTTPBody = "{\"uniqueKey\": \"1234\", \"firstName\": \"John\", \"lastName\": \"Doe\",\"mapString\": \"Mountain View, CA\", \"mediaURL\": \"https://udacity.com\",\"latitude\": 40.0, \"longitude\": -100.0}".dataUsingEncoding(NSUTF8StringEncoding)
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request) { data, response, error in
+            guard error == nil else {
+                print("Error returned by request", error)
+                return
+            }
+            print(NSString(data: data!, encoding: NSUTF8StringEncoding))
+        }
+        task.resume()
+    }
+    
     @IBAction func refreshButtonPushed(sender: AnyObject) {
         loadMapDataAndDisplay()
     }
