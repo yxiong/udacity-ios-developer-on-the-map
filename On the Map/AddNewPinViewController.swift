c4f2a54f1a8fd33b0b46059670e35e9276670896
Ying Xiong <ying@xiongs.org>
2015-11-01 21:25:32 -0800

    Add the map search logic

--- On the Map/AddNewPinViewController.swift
+++ On the Map/AddNewPinViewController.swift
@@ -7,13 +7,48 @@
 //
 
 import Foundation
+import MapKit
 import UIKit
 
 class AddNewPinViewController: UIViewController {
     @IBOutlet weak var locationTextField: UITextField!
+    @IBOutlet weak var mapView: MKMapView!
     
     @IBAction func findButtonPushed(sender: AnyObject) {
-        print("Finding: " + locationTextField.text!)
+        guard locationTextField.text != "" else {
+            let alert = UIAlertController(title: "Error", message: "Must enter a location.", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+            return
+        }
+        
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = locationTextField.text
+        request.region = mapView.region
+        let search = MKLocalSearch(request: request)
+        search.startWithCompletionHandler({(response, error) in
+            
+            guard error == nil else {
+                dispatch_async(dispatch_get_main_queue(), {
+                    let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertControllerStyle.Alert)
+                    alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default, handler: nil))
+                    self.presentViewController(alert, animated: true, completion: nil)
+                })
+                return
+            }
+            
+            guard response!.mapItems.count > 0 else {
+                dispatch_async(dispatch_get_main_queue(), {
+                    let alert = UIAlertController(title: "Error", message: "No Match Found.", preferredStyle: UIAlertControllerStyle.Alert)
+                    alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default, handler: nil))
+                    self.presentViewController(alert, animated: true, completion: nil)
+                })
+                return
+            }
+            
+            let item = response!.mapItems[0]
+            self.mapView.addAnnotation(item.placemark)
+        })
     }
     
     @IBAction func cancelButtonPushed(sender: AnyObject) {
