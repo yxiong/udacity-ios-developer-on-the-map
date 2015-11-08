06e919f70bf06aab920c17e9419020f762a81b71
Ying Xiong <ying@xiongs.org>
2015-11-07 17:56:06 -0800

    Use UIAlterController to handle error message

--- On the Map/LoginViewController.swift
+++ On the Map/LoginViewController.swift
@@ -12,29 +12,30 @@ class LoginViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var errorInfoTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        errorInfoTextField.text = ""
     }
 
     @IBAction func loginButtonClicked(sender: AnyObject) {
         // Make sure email and password are not empty.
         guard (!emailTextField.text!.isEmpty && !passwordTextField.text!.isEmpty) else {
-            errorInfoTextField.text! = "Email and/or password field is empty."
+            let alert = UIAlertController(title: "Error", message: "Email and/or password field is empty.", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
             return
         }
         OnTheMapModel.sharedInstance().login(emailTextField.text!, password: passwordTextField.text!) { (success, errorString) -> Void in
             guard success else {
                 dispatch_async(dispatch_get_main_queue(), {
-                    self.errorInfoTextField.text = errorString
+                    let alert = UIAlertController(title: "Error", message: errorString, preferredStyle: UIAlertControllerStyle.Alert)
+                    alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.Default, handler: nil))
+                    self.presentViewController(alert, animated: true, completion: nil)
                 })
                 return
             }
             
             dispatch_async(dispatch_get_main_queue(), {
-                self.errorInfoTextField.text = ""
                 let tabBarController = self.storyboard!.instantiateViewControllerWithIdentifier("TabBarController") as! UITabBarController
                 self.presentViewController(tabBarController, animated: true, completion: nil)
             })
